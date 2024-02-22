bfe56c3977a1493445cc3ba4ab7a5fd0af88ab60
imranhwebdev <alimrancv870@gmail.com>
2024-02-23 04:28:54 +0600

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -121,3 +121,22 @@ window.onscroll = function() {
 $(function(){
     $('#datepicker').datepicker();
   });
+
+
+// Swiper Slider 
+  var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 10,
+    slidesPerView: 3,
+    freeMode: true,
+    watchSlidesProgress: true,
+  });
+  var swiper2 = new Swiper(".mySwiper2", {
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+      swiper: swiper,
+    },
+  });
